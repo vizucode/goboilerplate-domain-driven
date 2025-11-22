6e41a212cdbb017b6885d70adfee5a37600529cf
Hafiz Iqbal Sahrunizar <havisikkubaru@gmail.com>
2025-11-22 22:44:00 +0700

    feat: Initialize use cases and remove prints
    Initialize use cases with repositories.
    
    Remove all prints in use cases.

--- internal/usecase/goods/create_goods.go
+++ internal/usecase/goods/create_goods.go
@@ -2,27 +2,29 @@ package goods
 
 import (
 	"context"
-	"fmt"
 	"goboilerplate-domain-driven/internal/domain/goods/entity.go"
 	"goboilerplate-domain-driven/internal/domain/goods/repository"
 )
 
 type CreateGoods struct {
-	repoOrder repository.GoodsRepository
+	repo repository.GoodsRepository
 }
 
-func NewCreateGoods() (resp *CreateGoods) {
-	return &CreateGoods{}
+func NewCreateGoods(
+	repo repository.GoodsRepository,
+) (resp *CreateGoods) {
+	return &CreateGoods{
+		repo: repo,
+	}
 }
 
 func (uc *CreateGoods) CreateGoods(ctx context.Context, req RequestGoods) (err error) {
 	p := entity.Goods{}
 
-	orders, err := uc.repoOrder.CreateGoods(ctx, &p)
+	_, err = uc.repo.CreateGoods(ctx, &p)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(orders.ID)
 	return nil
 }
--- internal/usecase/goods/get_order.go
+++ internal/usecase/goods/get_order.go
@@ -2,20 +2,23 @@ package goods
 
 import (
 	"context"
-	"fmt"
 	"goboilerplate-domain-driven/internal/domain/goods/repository"
 )
 
 type GetAllGoods struct {
-	repoOrder repository.GoodsRepository
+	repo repository.GoodsRepository
 }
 
-func NewGetAllOrder() (resp *GetAllGoods) {
-	return &GetAllGoods{}
+func NewGetAllOrder(
+	repo repository.GoodsRepository,
+) (resp *GetAllGoods) {
+	return &GetAllGoods{
+		repo: repo,
+	}
 }
 
 func (uc *GetAllGoods) GetGoods(ctx context.Context, id int) (resp ResponseGoods, err error) {
-	resultGoods, err := uc.repoOrder.GetGoods(ctx, id)
+	resultGoods, err := uc.repo.GetGoods(ctx, id)
 	if err != nil {
 		return resp, err
 	}
@@ -25,6 +28,5 @@ func (uc *GetAllGoods) GetGoods(ctx context.Context, id int) (resp ResponseGoods
 		Name: resultGoods.Name,
 	}
 
-	fmt.Println(resp)
 	return resp, err
 }
